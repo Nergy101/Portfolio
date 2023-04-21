d288b1e5dffa0255fbe028dd21f092fed0cb8752
Christian <chrisdvd@live.nl>
2023-04-21 23:58:56 +0200

    wip login

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons';
 import { Tile } from './tile.model';
+import PocketBase from 'pocketbase';
 
 @Component({
   selector: 'app-landing',
@@ -28,6 +29,27 @@ export class LandingComponent implements OnInit {
 
   async ngOnInit(): Promise<void> {
     await this.doCall();
+
+    const pb = new PocketBase('https://pocketbase.nergy.space');
+    // This method initializes a one-off realtime subscription and will
+    // open a popup window with the OAuth2 vendor page to authenticate.
+    //
+    // Once the external OAuth2 sign-in/sign-up flow is completed, the popup
+    // window will be automatically closed and the OAuth2 data sent back
+    // to the user through the previously established realtime connection.
+    const authData = await pb
+      .collection('users')
+      .authWithOAuth2({ provider: 'google' });
+
+    console.log(authData);
+
+    // after the above you can also access the auth data from the authStore
+    console.log(pb.authStore.isValid);
+    console.log(pb.authStore.token);
+    console.log(pb.authStore.model?.id);
+
+    // "logout" the last authenticated model
+    pb.authStore.clear();
   }
 
   async doCall(): Promise<void> {
