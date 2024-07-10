587a31b5e8184e34d40631354a99800f6a5c6df9
Nergy101 <cdijk4@gmail.com>
2024-07-10 11:13:09 +0200

    format

--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,32 +1,32 @@
-import { Component, OnInit } from '@angular/core';
-import { faChevronDown, faChevronUp } from '@fortawesome/free-solid-svg-icons';
-import { Tile } from './tile.model';
-import { MatSnackBar } from '@angular/material/snack-bar';
+import { Component, OnInit } from "@angular/core";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { faChevronDown, faChevronUp } from "@fortawesome/free-solid-svg-icons";
+import { Tile } from "./tile.model";
 
 @Component({
-  selector: 'app-landing',
-  templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.scss'],
+  selector: "app-landing",
+  templateUrl: "./landing.component.html",
+  styleUrls: ["./landing.component.scss"],
 })
 export class LandingComponent implements OnInit {
   tiles: Tile[] = [
-    { text: 'One', cols: 1, rows: 1, color: 'primary' },
-    { text: 'Two', cols: 1, rows: 1, color: 'secondary' },
-    { text: 'Three', cols: 2, rows: 1, color: 'accent' },
+    { text: "One", cols: 1, rows: 1, color: "primary" },
+    { text: "Two", cols: 1, rows: 1, color: "secondary" },
+    { text: "Three", cols: 2, rows: 1, color: "accent" },
   ];
   loading = false;
 
   weatherUrl =
-    'https://faas-ams3-2a2df116.doserverless.co/api/v1/web/fn-f15a95c0-61fb-478f-a954-1aa21586e126/cloud/getWeather';
+    "https://faas-ams3-2a2df116.doserverless.co/api/v1/web/fn-f15a95c0-61fb-478f-a954-1aa21586e126/cloud/getWeather";
 
   weatherResult?: any[];
 
-  query?= '?city=Utrecht';
+  query? = "?city=Utrecht";
 
   faChevronDown = faChevronDown;
   faChevronUp = faChevronUp;
 
-  constructor(private readonly snackBar: MatSnackBar) { }
+  constructor(private readonly snackBar: MatSnackBar) {}
 
   async ngOnInit(): Promise<void> {
     await this.doCall();
@@ -42,7 +42,9 @@ export class LandingComponent implements OnInit {
       }
     } catch (e) {
       console.error(e);
-      this.snackBar.open('Error while fetching weather data', 'Close', { duration: 3000 });
+      this.snackBar.open("Error while fetching weather data", "Close", {
+        duration: 3000,
+      });
     } finally {
       this.loading = false;
     }
@@ -51,10 +53,10 @@ export class LandingComponent implements OnInit {
   moveTo(divId: string) {
     document!
       .getElementById(divId)!
-      .scrollIntoView({ behavior: 'smooth', block: 'end' });
+      .scrollIntoView({ behavior: "smooth", block: "end" });
   }
 
   navigateTo(url: string) {
-    window.open(url, 'blank');
+    window.open(url, "blank");
   }
 }
