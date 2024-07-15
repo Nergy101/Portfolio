890617cfadbda0e508c27a9ad85b8276fd530bcc
Christian van Dijk <christian.vandijk@akzonobel.com>
2024-07-15 15:46:56 +0200

    devcontainer use gh cli

--- .devcontainer/scripts/postcreate.sh
+++ .devcontainer/scripts/postcreate.sh
@@ -5,15 +5,7 @@ BLUE='\033[34m'
 NC='\033[0m' # No Color
 
 echo -e "${BLUE}Configuring Git...${NC}"
-
-echo -e "${BLUE}Type Git Email:${NC}"
-read git_email
-
-echo -e "${BLUE}Type Git Username:${NC}"
-read git_username
-
-git config --global user.email $git_email
-git config --global user.name $git_username
+gh auth login
 
 echo -e "${BLUE}npm install...${NC}"
 npm install
