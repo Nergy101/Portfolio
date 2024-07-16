9a0523acfdbf50f5b7b15a9303ab768c83795ccf
Christian van Dijk <christian.vandijk@akzonobel.com>
2024-07-16 17:21:50 +0200

    devcontainer adjustments

--- .devcontainer/scripts/postattach.sh
+++ .devcontainer/scripts/postattach.sh
@@ -4,11 +4,8 @@ YELLOW='\033[0;33m'
 BLUE='\033[34m'
 NC='\033[0m' # No Color
 
-echo -e "${BLUE}Configuring Git...${NC}"
+echo -e "${BLUE}Configuring Git User...${NC}"
 gh auth login
-
-echo -e "${BLUE}npm install...${NC}"
-npm install
 
 echo -e "${BLUE}npm run start:devcontainer...${NC}"
 npm run start:devcontainer
---
+++ .devcontainer/scripts/postcreate.sh
@@ -0,0 +1,15 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+BLUE='\033[34m'
+NC='\033[0m' # No Color
+
+# transfer ownership of
+sudo chown -R node:node package.json README.md node_modules src tsconfig.app.json tsconfig.json
+sudo chmod -R 755 node_modules
+
+echo -e "${BLUE}Configure Git Repository...${NC}"
+git config --global --add safe.directory '*'
+
+echo -e "${BLUE}npm install...${NC}"
+npm install
