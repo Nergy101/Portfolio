ebac2be84f295f226cd83375aebf6bf42f301545
Christian <cdijk4@gmail.com>
2025-06-08 15:50:46 +0200

    fix: use styleUrls array in kofi dialog component

--- src/app/components/dialogs/kofi-dialog/kofi-dialog.component.ts
+++ src/app/components/dialogs/kofi-dialog/kofi-dialog.component.ts
@@ -3,6 +3,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-kofi-dialog',
     templateUrl: './kofi-dialog.component.html',
-    styleUrl: './kofi-dialog.component.scss'
+    styleUrls: ['./kofi-dialog.component.scss']
 })
 export class KofiDialogComponent {}
