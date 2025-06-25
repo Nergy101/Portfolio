69623864c12eb1f558f15be9a91f7b1c1d131471
Christian van Dijk <cdijk4@gmail.com>
2025-06-25 21:24:21 +0200

    Fixed iframes Remove unused router and menu components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,15 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
-import { KofiDialogComponent } from './components/dialogs/kofi-dialog/kofi-dialog.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LandingComponent } from './components/landing/landing.component';
-import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    MenuComponent,
-    LandingComponent,
-    FooterComponent,
-    KofiDialogComponent,
-  ],
+  imports: [HeaderComponent, LandingComponent, FooterComponent],
 })
 export class AppComponent {
   title = 'Nergy.space';
--- src/app/components/project-showcase/project-showcase.component.ts
+++ src/app/components/project-showcase/project-showcase.component.ts
@@ -1,13 +1,6 @@
-import { Component, inject, input, Input, OnDestroy } from '@angular/core';
+import { Component, inject, input, computed } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { DomSanitizer } from '@angular/platform-browser';
-import {
-  BehaviorSubject,
-  debounceTime,
-  distinctUntilChanged,
-  Subject,
-  takeUntil,
-} from 'rxjs';
 
 import { TechCardComponent } from '../tech-card/tech-card.component';
 
@@ -26,42 +19,18 @@ export interface ProjectTech {
   standalone: true,
   imports: [MatIconModule, TechCardComponent],
 })
-export class ProjectShowcaseComponent implements OnDestroy {
+export class ProjectShowcaseComponent {
   private readonly sanitizer = inject(DomSanitizer);
-  private readonly destroy$ = new Subject<void>();
-  private readonly iframeSrcSubject = new BehaviorSubject<string>('');
-  private debouncedSafeIframeSrc: string | null = null;
 
   title = input('');
   subtitle = input('');
   url = input('');
-  set iframeSrc(value: string) {
-    this.iframeSrcSubject.next(value);
-  }
-  get iframeSrc(): string {
-    return this.iframeSrcSubject.value;
-  }
+  iframeSrc = input('');
   badges = input<{ url: string; alt: string }[]>([]);
   techs = input<ProjectTech[]>([]);
 
-  constructor() {
-    this.iframeSrcSubject
-      .pipe(debounceTime(300), distinctUntilChanged(), takeUntil(this.destroy$))
-      .subscribe((src) => {
-        this.debouncedSafeIframeSrc =
-          this.sanitizer.bypassSecurityTrustResourceUrl(src) as string;
-      });
-  }
-
-  get safeIframeSrc() {
-    return (
-      this.debouncedSafeIframeSrc ||
-      this.sanitizer.bypassSecurityTrustResourceUrl(this.iframeSrc)
-    );
-  }
-
-  ngOnDestroy(): void {
-    this.destroy$.next();
-    this.destroy$.complete();
-  }
+  safeIframeSrc = computed(() => {
+    const src = this.iframeSrc();
+    return src ? this.sanitizer.bypassSecurityTrustResourceUrl(src) : null;
+  });
 }
